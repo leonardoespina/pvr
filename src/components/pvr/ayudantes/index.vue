dd1836b6ae4564aed805cb1d05d1b405986c4c9f
leonardoespina <leonardoespina@hotmail.com>
2023-08-12 22:53:52 -0400

    first commit

--- src/components/pvr/ayudantes/index.vue
+++ src/components/pvr/ayudantes/index.vue
@@ -1,42 +1,57 @@
 <template>
   <div class="q-gutter-md row items-start">
-    <q-input
-      v-model="model.cedula"
-      readonly="readonly"
-      type="text"
-      label="Cedula"
-      style="width: 250px"
-    />
-    <q-input
-      v-model="model.nombreApellido"
-      readonly="readonly"
-      type="text"
-      label="Nombre"
-      style="width: 250px"
-    />
-    <q-input
-      v-model="model.telefono"
-      readonly="readonly"
-      type="text"
-      label="Telefono"
-      style="width: 250px"
-    >
-      <template v-slot:after>
-        <q-btn
-          size="15px"
-          round
-          dense
-          flat
-          color="red"
-          icon="playlist_add"
-          @click="agregar"
-        />
-        <q-tooltip anchor="bottom middle" self="top middle" :offset="[10, 10]">
-          Agregar Acompañante o Ayudantes (<q-icon name="add" />)
-        </q-tooltip>
-      </template>
-    </q-input>
+    <form ref="form">
+      <q-select
+        filled
+        v-model="cedula"
+        use-input
+        hide-selected
+        fill-input
+        input-debounce="0"
+        :options="options"
+        @filter="filterFn"
+        hint="Indique Cedula"
+        style="width: 250px; padding-bottom: 32px"
+        @update:model-value="showChannel"
+      />
+      <q-input
+        v-model="model.nombreApellido"
+        readonly="readonly"
+        type="text"
+        ref="nombreApellido"
+        label="Nombre"
+        style="width: 250px"
+      />
+      <q-input
+        v-model="model.telefono"
+        readonly="readonly"
+        ref="telefono"
+        type="text"
+        label="Telefono"
+        style="width: 250px"
+      >
+        <template v-slot:after>
+          <q-btn
+            size="15px"
+            round
+            dense
+            flat
+            color="red"
+            icon="playlist_add"
+            @click="agregar(model)"
+          />
+          <q-tooltip
+            anchor="bottom middle"
+            self="top middle"
+            :offset="[10, 10]"
+          >
+            Agregar Acompañante o Ayudantes (<q-icon name="add" />)
+          </q-tooltip>
+        </template>
+      </q-input>
+    </form>
   </div>
+
   <Tabla :rows="rows" :col="col" :variable="variable" />
   <q-stepper-navigation>
     <q-btn @click="continuar(4)" color="primary" label="Continue" />
@@ -51,7 +66,7 @@
 </template>
 <script>
 import { loadList } from "../../../helper/list";
-import { ref } from "vue";
+import { ref, watchEffect } from "vue";
 
 import Tabla from "../../pvr/ayudantes/tables.vue";
 import { column } from "../../pvr/ayudantes/column";
@@ -65,8 +80,12 @@ export default {
     const stringOptions = ref([]),
       store = useStore(),
       col = ref(column()),
-      rows = ref(store.getters.isListAyudante),
+      rows = ref([]),
+      cedula = ref([]),
+      telefono = ref(null),
+      nombreApellido = ref(null),
       variable = "isListAyudante",
+      form = ref(""),
       model = ref([]);
 
     loadList("/api/ayudantes/All", "POST").then(
@@ -78,24 +97,30 @@ export default {
           };
         }))
     );
+
+    watchEffect(() => {
+      rows.value = store.getters.isListAyudante;
+    });
+
     const options = ref(stringOptions.value);
     const continuar = (v) => {
       let variable = { val: "isStep", valor: v };
       store.dispatch("varMutuacion", variable);
     };
 
-    const showChannel = (val) => {
-      let { value } = val.cedula;
-
-      loadList(`/api/ayudantesCedula/${value}`, "GET").then(
+    const showChannel = async (val) => {
+      await loadList(`/api/ayudantesCedula/${val.value}`, "GET").then(
         (datos) => (model.value = datos.data[0])
       );
     };
 
-    const agregar = () => {
-      rows.value.push(model.value);
+    const agregar = (model) => {
+      rows.value.push(model);
       let variable = { val: "isListAyudante", valor: rows.value };
       store.dispatch("varMutuacion", variable);
+      cedula.value = [];
+
+      form.value.reset;
     };
 
     return {
@@ -111,11 +136,14 @@ export default {
       options,
       model,
       continuar,
+      telefono,
+      nombreApellido,
       showChannel,
       col,
       rows,
       agregar,
       variable,
+      cedula,
     };
   },
 };
